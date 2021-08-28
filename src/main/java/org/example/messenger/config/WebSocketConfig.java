ff3309a41460a49ac5bd3cfb171269d539076f3c
mledium <mledium@mail.ru>
2021-08-28 19:09:43 +0300

    Initial commit

---
+++ src/main/java/org/example/messenger/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package org.example.messenger.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableScheduling
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+  @Override
+  public void registerStompEndpoints(final StompEndpointRegistry registry) {
+    registry.addEndpoint("/messenger").setAllowedOrigins("*").withSockJS();
+  }
+
+  @Override
+  public void configureMessageBroker(final MessageBrokerRegistry registry) {
+    registry.setApplicationDestinationPrefixes("/app");
+    registry.enableSimpleBroker("/topic");
+  }
+}
